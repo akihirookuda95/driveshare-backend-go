5c94885a0f04c601ebe45de7d3c4b7f348cac8fb
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-30 09:20:01 +0900

    [#1]Finished variable declaration

--- learning/lesson_variable_declaration.go
+++ learning/lesson_variable_declaration.go
@@ -1,15 +1,51 @@
 package main
 
-// "go doc" command is used to view documentation
-// example: go doc fmt Println
-import (
-	"fmt"
-	"os/user" // Importing the user package using / for absolute path
-	"time"
-)
+import "fmt"
+
+var a int = 1 // a is a global variable, declared outside a function
+// b := 2 // error: b is not declared outside a function
+
+func foo() {
+	// short variable declaration using inside only a function
+	xi := 1         // xi is a shorthand for var xi int = 1
+	fmt.Println(xi) // 1
+	xi = 2
+	fmt.Println(xi) // 2
+	var xf32 float32 = 1.2
+	xs := "Hello, World!"             // xs is a shorthand for var xs string = "Hello, World!"
+	xt, xf := true, false             // xt is a shorthand for var xt, xf bool = true, false
+	fmt.Println(xi, xf32, xs, xt, xf) // 1 1.2 Hello, World! true false
+	fmt.Printf("%T\n", xf32)          // float32, %T is a format specifier for type
+	fmt.Printf("%T\n", xi)            // int, \n is a format specifier for new line
+}
 
 func main() {
-	// Println is a public function in the fmt package
-	fmt.Println("Hello, Tom!", time.Now()) // Now() is a public function in the time package
-	fmt.Println(user.Current())            // Current() is a public function in the user package
+	// parenthesis () are used to group expressions inside and outside a function
+	var (
+		i    int     = 1
+		f64  float64 = 1.2
+		s    string  = "Hello, World!"
+		t, f bool    = true, false // using , to declare multiple variables
+	)
+	fmt.Println(i, f64, s, t, f)
+
+	//var (
+	//	i    int     // 0 is the default value for int
+	//	f64  float64 // 0.0 is the default value for float64
+	//	s    string  // "" is the default value for string
+	//	t, f bool    // false is the default value for bool
+	//)
+	//fmt.Println(i, f64, s, t, f)
+
+	// short variable declaration using inside only a function
+	xi := 1               // xi is a shorthand for var xi int = 1
+	xf64 := 1.2           // xf64 is a shorthand for var xf64 float64 = 1.2
+	xs := "Hello, World!" // xs is a shorthand for var xs string = "Hello, World!"
+	xt, xf := true, false // xt is a shorthand for var xt, xf bool = true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T\n", xf64) // float64, %T is a format specifier for type
+
+	//fmt.Println(a) // 1, a is a global variable
+
+	foo()
 }
