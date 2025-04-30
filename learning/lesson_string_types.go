60b3893fed7c66aa6c880fd7b48a211d99b0e338
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-30 13:18:53 +0900

    [#1]Finished boolean types

--- learning/lesson_string_types.go
+++ learning/lesson_string_types.go
@@ -1,25 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func main() {
-	fmt.Println("Hello, World!")      // Hello, World!
-	fmt.Println("Hello, " + "World!") // Hello, World!
-	fmt.Println("Hello, World!"[0])   // 72, ASCII code for 'H'
-
-	var s string = "Hello, World!"
-	fmt.Println(s) // Hello, World!
-	// s[0] = 'h' // error: cannot assign to s[0]
-	var sLowerCase string = strings.Replace(s, "H", "h", 1) // Hello, World!, strings.Replace() does not modify the original string
-	fmt.Println(s)                                          // Hello, World! // s is still the same
-	fmt.Println(sLowerCase)                                 // hello, World! // sLowerCase is a new string
-	s = sLowerCase
-	fmt.Println(s) // hello, World! // s is now the new string
-
-	fmt.Println(strings.Contains(s, "Hello")) // false
-
-	fmt.Println("Test" + "ing") // Testing, string concatenation using + operator
-}
+//func main() {
+//	fmt.Println("Hello, World!")      // Hello, World!
+//	fmt.Println("Hello, " + "World!") // Hello, World!
+//	fmt.Println("Hello, World!"[0])   // 72, ASCII code for 'H'
+//
+//	var s string = "Hello, World!"
+//	fmt.Println(s) // Hello, World!
+//	// s[0] = 'h' // error: cannot assign to s[0]
+//	var sLowerCase string = strings.Replace(s, "H", "h", 1) // Hello, World!, strings.Replace() does not modify the original string
+//	fmt.Println(s)                                          // Hello, World! // s is still the same
+//	fmt.Println(sLowerCase)                                 // hello, World! // sLowerCase is a new string
+//	s = sLowerCase
+//	fmt.Println(s) // hello, World! // s is now the new string
+//
+//	fmt.Println(strings.Contains(s, "Hello")) // false
+//
+//	fmt.Println("Test" + "ing") // Testing, string concatenation using + operator
+//}
