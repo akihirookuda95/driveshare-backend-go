02642e1ba9e7fcba23ba3f0729370a0d1535a8af
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-30 14:04:26 +0900

    [#1]Finished make and cap of slice

--- learning/lesson_slice.go
+++ learning/lesson_slice.go
@@ -1,30 +1,28 @@
 package main
 
-import "fmt"
-
-func main() {
-	n := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(n)      // [1 2 3 4 5 6]
-	fmt.Println(n[2])   // 3
-	fmt.Println(n[2:4]) // [3 4]
-	fmt.Println(n[:2])  // [1 2]
-	fmt.Println(n[2:])  // [3 4 5 6]
-	fmt.Println(n[:])   // [1 2 3 4 5 6]
-
-	n[2] = 100
-	fmt.Println(n) // [1 2 100 4 5 6]
-
-	// slice inside slice
-	var board = [][]int{
-		[]int{1, 2, 3},
-		[]int{3, 4, 5},
-		[]int{5, 6, 7},
-	}
-	fmt.Println(board) // [[1 2 3] [3 4 5] [5 6 7]]
-
-	n = append(n, 100)
-	fmt.Println(n) // [1 2 100 4 5 6 100]
-	n = append(n, 100, 200, 300, 400, 500)
-	fmt.Println(n) // [1 2 100 4 5 6 100 200 300 400 500]
-
-}
+//func main() {
+//	n := []int{1, 2, 3, 4, 5, 6}
+//	fmt.Println(n)      // [1 2 3 4 5 6]
+//	fmt.Println(n[2])   // 3
+//	fmt.Println(n[2:4]) // [3 4]
+//	fmt.Println(n[:2])  // [1 2]
+//	fmt.Println(n[2:])  // [3 4 5 6]
+//	fmt.Println(n[:])   // [1 2 3 4 5 6]
+//
+//	n[2] = 100
+//	fmt.Println(n) // [1 2 100 4 5 6]
+//
+//	// slice inside slice
+//	var board = [][]int{
+//		[]int{1, 2, 3},
+//		[]int{3, 4, 5},
+//		[]int{5, 6, 7},
+//	}
+//	fmt.Println(board) // [[1 2 3] [3 4 5] [5 6 7]]
+//
+//	n = append(n, 100)
+//	fmt.Println(n) // [1 2 100 4 5 6 100]
+//	n = append(n, 100, 200, 300, 400, 500)
+//	fmt.Println(n) // [1 2 100 4 5 6 100 200 300 400 500]
+//
+//}
