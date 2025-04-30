dcdd8128bd60ba797e563ca547ca717d5f2f28f7
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-30 10:02:49 +0900

    [#1]Finished numeric types

--- learning/lesson_const.go
+++ learning/lesson_const.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 const Pi = 3.14 // Pi is a constant, declared outside a function
 const (
 	Username = "admin" // Username is a constant, declared outside a function
@@ -15,6 +11,7 @@ var big int = 9223372036854775807 // the maximum value for int64
 // var bigVar int = 9223372036854775807 + 1 // error: constant 9223372036854775808 overflows int
 const bigConst = 9223372036854775807 + 1 // no error, constant is not evaluated at runtime
 
+/*
 func main() {
 	fmt.Println(Username, Password) // Username and Password are constants
 	// Pi = 3 // error: cannot assign to Pi
@@ -26,3 +23,5 @@ func main() {
 	fmt.Println(bigConst - 1) // 9223372036854775807, no error because constant is not evaluated at runtime
 
 }
+
+*/
