d893a628656aa3bfa3f15756a437c21751e09444
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-05-01 08:24:55 +0900

    [#1]Finished if statement

--- learning/lesson_variadic_arguments.go
+++ learning/lesson_variadic_arguments.go
@@ -10,13 +10,13 @@ func foo(params ...int) {
 	}
 }
 
-func main() {
-	foo()           // 0 []
-	foo(10, 20)     // 2 [10 20] 10 20
-	foo(10, 20, 30) // 3 [10 20 30] 10 20 30
-
-	s := []int{1, 2, 3}
-	fmt.Println(s) // [1 2 3]
-	foo(s...)      // 3 [1 2 3] 1 2 3, ... is used to unpack the slice
-	// foo(s) // error: cannot use s (type []int) as type int in argument to foo
-}
+//func main() {
+//	foo()           // 0 []
+//	foo(10, 20)     // 2 [10 20] 10 20
+//	foo(10, 20, 30) // 3 [10 20 30] 10 20 30
+//
+//	s := []int{1, 2, 3}
+//	fmt.Println(s) // [1 2 3]
+//	foo(s...)      // 3 [1 2 3] 1 2 3, ... is used to unpack the slice
+//	// foo(s) // error: cannot use s (type []int) as type int in argument to foo
+//}
