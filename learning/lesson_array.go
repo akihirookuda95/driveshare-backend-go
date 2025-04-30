c8a571f4e71219bb686995e476f76079e261fcc3
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-04-30 13:42:30 +0900

    [#1]Finished slice

--- learning/lesson_array.go
+++ learning/lesson_array.go
@@ -1,21 +1,19 @@
 package main
 
-import "fmt"
-
-func main() {
-
-	// array session
-	var a [2]int // array of 2 integers
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a) // [100 200]
-
-	//var b [2]int = [2]int{100, 200} // array of 2 integers, can not be changed size after declaration
-	//fmt.Println(b)              // [100 200]
-
-	// slice session
-	var b []int = []int{100, 200} // slice of integers, can be changed size after declaration
-	fmt.Println(b)                // [100 200]
-	b = append(b, 300)
-	fmt.Println(b) // [100 200 300]
-}
+//func main() {
+//
+//	// array session
+//	var a [2]int // array of 2 integers
+//	a[0] = 100
+//	a[1] = 200
+//	fmt.Println(a) // [100 200]
+//
+//	//var b [2]int = [2]int{100, 200} // array of 2 integers, can not be changed size after declaration
+//	//fmt.Println(b)              // [100 200]
+//
+//	// slice session
+//	var b []int = []int{100, 200} // slice of integers, can be changed size after declaration
+//	fmt.Println(b)                // [100 200]
+//	b = append(b, 300)
+//	fmt.Println(b) // [100 200 300]
+//}
