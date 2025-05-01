bd84951ec2b613e5e5aa8ebc38110af8b92b9489
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-05-01 19:49:02 +0900

    [#1]Finished error handling

--- learning/lesson_logging.go
+++ learning/lesson_logging.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -15,18 +14,18 @@ func loggingSettings(logFile string) {
 	log.SetOutput(multiLogFile)
 }
 
-func main() {
-	loggingSettings("learn.log")    // log file name
-	_, err := os.Open("ldjlsfjlaj") // not exist file
-	if err != nil {
-		log.Println("error:", err) // log is a package that provides simple logging functions
-		log.Fatalln("error:", err) // exits the program with a non-zero status code
-	}
-
-	log.Println("logging!")             // log is a package that provides simple logging functions
-	log.Printf("%T %v", "test", "test") // string test, %T is the type of the variable, %v is the value of the variable
-
-	log.Fatalf("%T %v", "test", "test") // string test
-	log.Fatalln("error!")               // exits the program with a non-zero status code
-	fmt.Println("OK!")                  // this line will not be executed because log.Fatalln() exits the program
-}
+//func main() {
+//	loggingSettings("learn.log")    // log file name
+//	_, err := os.Open("ldjlsfjlaj") // not exist file
+//	if err != nil {
+//		log.Println("error:", err) // log is a package that provides simple logging functions
+//		log.Fatalln("error:", err) // exits the program with a non-zero status code
+//	}
+//
+//	log.Println("logging!")             // log is a package that provides simple logging functions
+//	log.Printf("%T %v", "test", "test") // string test, %T is the type of the variable, %v is the value of the variable
+//
+//	log.Fatalf("%T %v", "test", "test") // string test
+//	log.Fatalln("error!")               // exits the program with a non-zero status code
+//	fmt.Println("OK!")                  // this line will not be executed because log.Fatalln() exits the program
+//}
