344abc8f4601f5c35f65688674e0ed6585ec5534
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-05-01 11:06:59 +0900

    [#1]Finished switch statement

---
+++ learning/lesson_switch.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func getOsName() string {
+	return "mac"
+}
+func main() {
+	// normal switch statement, os variable is usable outside and inside the switch statement
+	//os := getOsName()
+	//switch os {
+	//case "linux":
+	//	fmt.Println("Linux")
+	//case "windows":
+	//	fmt.Println("Windows")
+	//case "mac":
+	//	fmt.Println("Mac")
+	//default:
+	//	fmt.Println("Unknown OS")
+	//}
+
+	// simple switch statement, os variable is usable only inside the switch statement
+	switch os := getOsName(); os {
+	case "linux":
+		fmt.Println("Linux")
+	case "windows":
+		fmt.Println("Windows")
+	case "mac":
+		fmt.Println("Mac")
+	default:
+		fmt.Println("Unknown OS, as is", os)
+	}
+
+	t := time.Now()
+	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning")
+	case t.Hour() < 18:
+		fmt.Println("Good afternoon")
+	}
+
+}
