811ee43f5071e9a0dfdcc30c7503c4cc85d424e7
Akihiro.Okuda <ScuderiaFerrariSebastian@gmail.com>
2025-05-01 10:58:23 +0900

    [#1]Finished range

--- learning/lesson_for.go
+++ learning/lesson_for.go
@@ -1,30 +1,28 @@
 package main
 
-import "fmt"
-
-func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("continue")
-			continue // skip the rest of the loop when i is 3
-		}
-		if i > 5 {
-			fmt.Println("break")
-			break // exit the loop when i is greater than 5
-		}
-		fmt.Println(i)
-	}
-
-	sum := 1
-	// we can skip initialization and increment in the for loop
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
-	}
-	fmt.Println(sum)
-
-	// infinite loop
-	//for {
-	//	fmt.Println("Hello")
-	//}
-}
+//func main() {
+//	for i := 0; i < 10; i++ {
+//		if i == 3 {
+//			fmt.Println("continue")
+//			continue // skip the rest of the loop when i is 3
+//		}
+//		if i > 5 {
+//			fmt.Println("break")
+//			break // exit the loop when i is greater than 5
+//		}
+//		fmt.Println(i)
+//	}
+//
+//	sum := 1
+//	// we can skip initialization and increment in the for loop
+//	for sum < 10 {
+//		sum += sum
+//		fmt.Println(sum)
+//	}
+//	fmt.Println(sum)
+//
+//	// infinite loop
+//	//for {
+//	//	fmt.Println("Hello")
+//	//}
+//}
